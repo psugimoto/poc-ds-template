b7c7043a6e493166d7fa05232f75c559b9e357db
Alan Justino <alan.justino@yahoo.com.br>
2017-04-07 18:16:15 -0300

    Convert Flask tests to Django

--- mlapp/tests.py
+++ mlapp/tests.py
@@ -1,3 +1,39 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
 from django.test import TestCase
 
-# Create your tests here.
+from .management.commands import train
+
+import pytest
+
+
+def test_dumb():
+    assert 1+1 == 2
+
+
+class TestIndex(TestCase):
+    def setUp(self):
+        train.Command().handle()
+        self.client.post('/load')
+
+    def test_load_model(self):
+        # 0: Endpoints list
+        response = self.client.post('/load')
+        assert response.status_code == 200
+
+    def test_evalueate_model(self):
+        response = self.client.get('/evaluate')
+        assert response.status_code == 200
+        assert 'precision' in response.json()
+
+    def test_predict(self):
+        response = self.client.post('/predict', {
+            "sepal length": 5,
+            "sepal width": 3,
+            "petal length": 3,
+            "petal width": 1,
+        })
+        assert response.status_code == 200
+        assert 'predicted' in response.json()
+        assert response.json()['predicted'] in {'Virginica', 'Versicolour', 'Setosa'}
